220304f7aa5110ce7054349bf0e75356abb3162d
MeriFeoktistova <gingeer2805@gmail.com>
2019-09-22 23:06:01 +0300

    добавлены цвета

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-window.GRID = initGrid(20, 20);
+GRID = initGrid(20, 20);
 
-GRID[4][7].style.background = "#48a"
+const blue = "#48a";
+const green = "#152";
+const orange = "#f51";
+const yellow = "#fa0";
+const brown = "#a52";
+
+
+GRID[4][7].style.background = blue;
+GRID[4][9].style.background = green;
+GRID[5][8].style.background = orange;
+GRID[6][7].style.background = yellow;
+GRID[6][9].style.background = brown;
+
+
